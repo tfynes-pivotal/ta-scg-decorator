0b5e537c484922295df4488d0e9a5973a3adf012
Mark Fynes <tfynes@pivotal.io>
2021-07-27 14:43:10 -0400

    ...

--- deploy-gateway.sh
+++ deploy-gateway.sh
@@ -2,4 +2,4 @@
 kubectl apply -f ./gateway-config.yaml
 kubectl apply -f ./route-config.yaml
 kubectl apply -f ./mapping.yaml
-kubect expose pod ServiceName-gateway-0 --port=80 --target-port=8080 --type=LoadBalancer
+kubectl expose pod ServiceName-gateway-0 --port=80 --target-port=8080 --type=LoadBalancer
