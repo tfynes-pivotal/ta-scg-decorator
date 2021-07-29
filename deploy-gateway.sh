2a66532b69975341cbaaf1c764e2fc4c3c800a97
Mark Fynes <tfynes@pivotal.io>
2021-07-29 16:35:30 -0400

    openapi api doc exposed

--- deploy-gateway.sh
+++ deploy-gateway.sh
@@ -2,8 +2,9 @@
 kubectl apply -f ./gateway-config.yaml
 kubectl apply -f ./route-config.yaml
 kubectl apply -f ./mapping.yaml
+kubectl -n spring-cloud-gateway patch src scg-operator -p '{"spec":{"type":"LoadBalancer"}}'
 kubectl expose pod ServiceName-gateway-0 --port=80 --target-port=8080 --type=LoadBalancer
-echo -n Waiting for external IP
+echo -n Waiting for external IPs
 until [ -n "$(kubectl get svc ServiceName-gateway-0 -o jsonpath='{.status.loadBalancer.ingress[0].ip}')" ]; do
     sleep 2
     echo -n .
@@ -12,3 +13,9 @@ echo
 echo Test Access to Managed Service
 echo
 echo curl http://$(kubectl get svc ServiceName-gateway-0 -o jsonpath='{.status.loadBalancer.ingress[0].ip}')/ServiceName
+echo
+until [ -n "$(kubectl -n spring-cloud-gateway get svc scg-operator -o jsonpath='{.status.loadBalancer.ingress[0].ip}')" ]; do
+    sleep 2
+echo Observe openapi documentation for exposed APIs here; http://$(kubectl -n spring-cloud-gateway get svc scg-operator -o jsonpath='{.status.loadBalancer.ingress[0].ip}')/openapi
+echo
+
