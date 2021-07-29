0aa3edd086a52495470db98330fa7886878e664a
Mark Fynes <tfynes@pivotal.io>
2021-07-29 17:48:01 -0400

    ...

--- register-with-apiportal.sh
+++ register-with-apiportal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo Register Spring Cloud Gateway Operator with API Portal
-kubectl -n api-portal set env deployment.apps/api-portal-server API_PORTAL_SOURCE_URLS="https://petstore.swagger.io/v2/swagger.json, https://petstore3.swagger.io/api/v3/openapi.json,$(kubectl -n spring-cloud-gateway get svc scg-operator -o jsonpath='{.status.loadBalancer.ingress[0].ip}')/openapi"
+kubectl -n api-portal set env deployment.apps/api-portal-server API_PORTAL_SOURCE_URLS="https://petstore.swagger.io/v2/swagger.json, https://petstore3.swagger.io/api/v3/openapi.json,http://$(kubectl -n spring-cloud-gateway get svc scg-operator -o jsonpath='{.status.loadBalancer.ingress[0].ip}')/openapi"
 
 kubectl -n api-portal rollout restart deployment api-portal-server
 
